c403fc7f99b18303c21fc29d9c1eb9fec4617fee
Sahid malik <106477157+Sahidmalik001@users.noreply.github.com>
2024-02-11 08:31:59 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,24 +1,40 @@
 # (c) @PredatorHackerzZ || @TeleRoidGroup
 
-import os
+import os, re
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
 class Config(object):
+	ID = {}
 	API_ID = int(os.environ.get("API_ID", "977080"))
 	API_HASH = os.environ.get("API_HASH", "0c20c4265501492a1513f91755acd42b")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5991682088:AAFKgMtrZPYy_PeXN_F4Zs4v1n2lJd8nCgY")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME", "thunder_store3_bot")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001896983424"))
-	SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "moneykamalo.com")
-	SHORTLINK_API = os.environ.get('SHORTLINK_API', "c176f08934f241bd3980fcd7b940611ad2cbb3ce")
+	SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "omegalinks.in")
+	SHORTLINK_API = os.environ.get('SHORTLINK_API', "4b392f8eb6ad7113fbe589ae3b1e7cf0eac597b6")
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "399726799"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://abcd:abcd@cluster0.zglig1z.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001577615410")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001896983424")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001896983424") 
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
-	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True)) 
+	# verification 
+	VR_SITE = os.environ.get('VR_SITE', "moneykamalo.com")
+	VR_API = os.environ.get('VR_API', "c176f08934f241bd3980fcd7b940611ad2cbb3ce")
+	VERIFY = is_enabled((os.environ.get('VERIFY', "True")), True)	
+	VERIFY_TXT = """Hey {},\n\nʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴠᴇʀɪғɪᴇᴅ ᴛᴏᴅᴀʏ. ᴘʟᴇᴀsᴇ ᴠᴇʀɪғʏ ᴀɴᴅ ɢᴇᴛ ᴜɴʟɪᴍɪᴛᴇᴅ ᴀᴄᴄᴇss ғᴏʀ ᴏɴᴇ ᴅᴀʏ\n\n<b>इस बॉट को इस्तेमाल करने के लिए आपको रोजाना 1 बार verify करना होगा नहीं तो आप इसका इस्तेमाल नहीं कर पाएंगे"""
+	VERIFY_COMPLETE_TEXT = """Hay. {}.\n\nʏᴏᴜ ᴀʀᴇ ɴᴏᴡ ᴠᴇʀɪғɪᴇᴅ ғᴏʀ ᴛᴏɴɪɢʜᴛ 12:00ᴀᴍ ... ᴇɴɪᴏʏ ʏᴏᴜʀ ᴜɴʟɪᴍɪᴛᴇᴅ ᴀᴄᴄᴇꜱꜱ ғᴏʀ ʏᴏᴜʀ ᴇɴᴛᴇʀᴛᴀɪɴᴍᴇɴᴛ🧑‍🎤 ...\n\n#Completed"""	
 	ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot of Filmyfunda Movies 
 
