1eaf235ab473f9e3b692e3711822801ee35614b5
Sahid malik <106477157+Sahidmalik001@users.noreply.github.com>
2024-02-11 08:28:47 +0530

    Update database.py

--- handlers/database.py
+++ handlers/database.py
@@ -1,5 +1,5 @@
 # (c) @PredatorHackerzZ
-
+import pytz
 import datetime
 import motor.motor_asyncio
 from configs import Config
@@ -11,7 +11,9 @@ class Database:
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
-
+        self.verify_id = self.db.verify_id
+        self.misc = self.db.misc
+    
     def new_user(self, id):
         return dict(
             id=id,
@@ -74,6 +76,66 @@ class Database:
     async def get_all_banned_users(self):
         banned_users = self.col.find({'ban_status.is_banned': True})
         return banned_users
+
+#added single verify 
+
+    async def get_notcopy_user(self, user_id):
+        user_id = int(user_id)
+
+        user = await self.misc.find_one({"user_id": user_id})
+        ist_timezone = pytz.timezone('Asia/Kolkata')
+
+        if not user:
+            res = {
+                "user_id": user_id,
+                "last_verified": datetime.datetime(2020, 5, 17, 0, 0, 0, tzinfo=ist_timezone),
+            }
+
+            user = await self.misc.insert_one(res)
+
+        return user
+
+    async def update_notcopy_user(self, user_id, value:dict):
+        user_id = int(user_id)
+        myquery = {"user_id": user_id}
+        newvalues = { "$set": value }
+        await self.misc.update_one(myquery, newvalues)
+
+    async def is_user_verified(self, user_id):
+        user = await self.get_notcopy_user(user_id)
+        try:
+            pastDate = user["last_verified"]
+        except Exception:
+            user = await self.get_notcopy_user(user_id)
+            pastDate = user["last_verified"]
+
+        ist_timezone = pytz.timezone('Asia/Kolkata')
+        pastDate = pastDate.astimezone(ist_timezone)
+        current_time = datetime.datetime.now(tz=ist_timezone)
+
+        seconds_since_midnight = (current_time - datetime.datetime(current_time.year, current_time.month, current_time.day, 0, 0, 0, tzinfo=ist_timezone)).total_seconds()
+
+        # Calculate the difference between the two times
+        time_diff = current_time - pastDate
+
+        # Get the total number of seconds between the two times
+        total_seconds = time_diff.total_seconds()
+        return total_seconds <= seconds_since_midnight
+            
+    async def create_verify_id(self, user_id: int, hash):
+        res = {"user_id": user_id, "hash":hash, "verified":False}
+        return await self.verify_id.insert_one(res)
+
+    async def get_verify_id_info(self, user_id: int, hash):
+        return await self.verify_id.find_one({"user_id": user_id, "hash": hash})
+    
+    async def update_verify_id_info(self, user_id, hash, value: dict):
+        myquery = {"user_id": user_id, "hash": hash}
+        newvalues = { "$set": value }
+        return await self.verify_id.update_one(myquery, newvalues)
+
+
+
 
 
 db = Database(Config.DATABASE_URL, Config.BOT_USERNAME)
