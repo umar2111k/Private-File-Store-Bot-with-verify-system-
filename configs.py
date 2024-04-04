7cab2905623b5b3e25850cac3d6ba14557066d5d
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2024-04-04 17:12:44 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -25,8 +25,8 @@ class Config(object):
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001336576681")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001896983424") 
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
-	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True)) 
 	# verification 
@@ -50,7 +50,7 @@ This is a Permanent FileStore Bot of Filmyfunda Movies
 │
 ├🔸 **Developer:** [Ded eye](https://t.me/ded_eye) 
 │
-├🔸 **movies Updates:** [Movies Channel](https://t.me/filmyfunda_movies)
+├🔸 **Update Channel:** [Movies Channel](https://t.me/illegal_developer)
 │
 ╰──────[ 😎 ]───────────⍟
 """
