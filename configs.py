70dc7f5b1bf5d23a469d9d78d5bd45f3685bc380
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2024-04-08 16:55:57 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -58,5 +58,13 @@ This is a Permanent FileStore Bot By Illegal Developer
  I am Super noob Please Support My Hard Work.
 """
 	HOME_TEXT = """
-Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**..
+Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
+
+<b><u>How to Use Bot & it's Benefits??</b></u>
+
+📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
+
+⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+
+❌ <b>PORNOGRAPHY CONTENTS</b> are strictly prohibited & get Permanent Ban..
 """
